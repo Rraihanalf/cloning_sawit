ee5413d1748ca930ec9e4fc16294163a50e35633
Rraihanalf <rraihan.alf@gmail.com>
2024-05-14 17:53:57 +0800

    Final Database Migration Dataseed

---
+++ database/migrations/2024_05_14_063234_create_jadwals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwals', function (Blueprint $table) {
+            // $table->id();
+            $table->string('id_jadwal')->primary();
+            $table->string('id_lapangan');
+            $table->date('tgl_kerja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jadwals');
+    }
+};
